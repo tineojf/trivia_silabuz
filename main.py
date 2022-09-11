31815fd510879c9b07e169f5b5e89abf3cdc738f
Tineo <tineo.dev@gmail.com>
2022-09-10 20:12:45 -0500

    Start -  proyecto trivia

---
+++ main.py
@@ -0,0 +1,43 @@
+
+###todo Trivia
+
+
+#* Import
+#! Agregar
+
+#* Colores
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+NORMALIZE = '\033[39m'
+
+
+###todo Bienvenida de Trivia
+print(YELLOW + "Trivia de Fórmula 1" + NORMALIZE)
+
+nombreParticipante = input(MAGENTA + "¿Cuál es tu nombre? " + NORMALIZE)
+print(YELLOW + f"Bienvenido {nombreParticipante}\nAplica tus conocimientos y obtén el máximo puntaje. ¡Suerte!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
